67d0c93a080095f653799c7e9f7450ed09df0007
Ramesh Nair <ram@hiddentao.com>
2012-04-01 17:00:56 +0100

    Initial commit - v1.0.0

---
+++ README.md
@@ -0,0 +1,81 @@
+# Google TTS
+
+A Javascript library to enable Text-to-Speech functionality in your web apps. This is essentially an API for Google's
+Text-to-Speech engine and is based on code at http://weston.ruter.net/projects/google-tts/.
+
+## Features
+
+* Converts upto 42 languages
+* Supports playback through [HTML5 Audio tag](https://developer.mozilla.org/En/HTML/Element/Audio) if available in browser.
+* Asynchronous playback API
+* Small and compact: <1 KB minifed and gzipped
+
+## Installation
+
+Add the following inside your HTML `<body>` tag, near the bottom:
+
+    <script type="text/javascript" src="https://raw.github.com/hiddentao/google-tts/google-tts.min.js"></script>
+
+## Usage
+
+Once the script is loaded the following API is available to you:
+
+### Initialization
+
+    var tts = new GoogleTTS();
+
+This initializes a new instance of the library with the default language set to _English (en)_. To set a different
+default language, pass in the language code during initialization:
+
+    var tts = new GoogleTTS('fr');   // French
+
+### languages()
+
+Get the full list of supported languages.
+
+**Returns:** An object such as:
+
+    {
+        ...
+        'en' : 'English',
+        'zh-CN' : 'Mandarin (simplified)',
+        ...
+    }
+
+### canPlay()
+
+Get whether the Text-to-Speech audio can be played by the browser.
+
+**Returns:** `true` if so, `false` otherwise.
+
+### url(text, language)
+
+Construct the URL to fetch the speech audio for given text and language.
+
+**Params:**
+  * `text` - the text to convert to speech.
+  * `language` - the language to speak it in. If omitted then the default language (see above) is assumed.
+
+**Returns:** a URL to the audio file.
+
+### play(text, language)
+
+Fetch and play the speech audio for given text and language.
+
+**Params:**
+  * `text` - the text to convert to speech.
+  * `language` - the language to speak it in. If omitted then the default language (see above) is assumed.
+  * `cb` - Completion callback function with signature `(err)`, where `err` holds information on any errors which may occur.
+
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2012 Ramesh Nair &lt;www.hiddentao.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ google-tts.js
@@ -0,0 +1,173 @@
+/*! google-tts v1.0.0 | https://github.com/hiddentao/google-tts */
+(function (name, definition){
+  if ('function' === typeof define){ // AMD
+    define(definition);
+  } else if ('undefined' !== typeof module && module.exports) { // Node.js
+    module.exports = definition();
+  } else { // Browser
+    var theModule = definition(), global = this, old = global[name];
+    theModule.noConflict = function () {
+      global[name] = old;
+      return theModule;
+    };
+    global[name] = theModule;
+  }
+})('GoogleTTS', function () {
+  /**
+   * The API instance.
+   *
+   * @param defaultLanguage Optional. The language to use when not specified. 'en' is default.
+   * @constructor
+   */
+  return function(defaultLanguage) {
+    /**
+     * Default language (code).
+     * @type {String}
+     */
+    defaultLanguage = defaultLanguage || 'en';
+
+    /**
+     * Whether we can play MP3 audio or not. Gets set further down.
+     * @type {Boolean}
+     */
+    var canPlayHTML5MP3Audio = undefined;
+
+    /**
+     * Full list of languages.
+     * @type {Object}
+     */
+    var languages = {
+      'af' : 'Afrikaans',
+      'sq' : 'Albanian',
+      'ar' : 'Arabic',
+      'hy' : 'Armenian',
+      'ca' : 'Catalan',
+      'zh-CN' : 'Mandarin (simplified)',
+      'zh-TW' : 'Mandarin (traditional)',
+      'hr' : 'Croatian',
+      'cs' : 'Czech',
+      'da' : 'Danish',
+      'nl' : 'Dutch',
+      'en' : 'English',
+      'eo' : 'Esperanto',
+      'fi' : 'Finnish',
+      'fr' : 'French',
+      'de' : 'German',
+      'el' : 'Greek',
+      'ht' : 'Haitian Creole',
+      'hi' : 'Hindi',
+      'hu' : 'Hungarian',
+      'is' : 'Icelandic',
+      'id' : 'Indonesian',
+      'it' : 'Italian',
+      'ja' : 'Japanese',
+      'ko' : 'Korean',
+      'la' : 'Latin',
+      'lv' : 'Latvian',
+      'mk' : 'Macedonian',
+      'no' : 'Norwegian',
+      'pl' : 'Polish',
+      'pt' : 'Portuguese',
+      'ro' : 'Romanian',
+      'ru' : 'Russian',
+      'sr' : 'Serbian',
+      'sk' : 'Slovak',
+      'es' : 'Spanish',
+      'sw' : 'Swahili',
+      'sv' : 'Swedish',
+      'ta' : 'Tamil',
+      'th' : 'Thai',
+      'tr' : 'Turkish',
+      'vi' : 'Vietnamese',
+      'cy' : 'Welsh'
+   };
+
+
+    /**
+     * Check if we can play MP3 Audio using the HMTL5 Audio tag.
+     */
+    (function(cb) {
+      try {
+        var audio = new Audio();
+
+        //Shortcut which doesn't work in Chrome (always returns ""); pass through
+        // if "maybe" to do asynchronous check by loading MP3 data: URI
+        if('probably' === audio.canPlayType('audio/mpeg'))
+          cb(null, true);
+
+        //If this event fires, then MP3s can be played
+        audio.addEventListener('canplaythrough', function() {
+          cb(null, true);
+        }, false);
+
+        //If this is fired, then client can't play MP3s
+        audio.addEventListener('error', function() {
+          cb(this.error, false)
+        }, false);
+
+        //Smallest base64-encoded MP3 I could come up with (less than 0.000001 seconds long)
+        audio.src = "data:audio/mpeg;base64,/+MYxAAAAANIAAAAAExBTUUzLjk4LjIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        audio.load();
+      }
+      catch(e){
+        cb(e, false);
+      }
+    })(function (err, canPlay) {
+      canPlayHTML5MP3Audio = canPlay;
+    });
+
+    /**
+     * Get supported languages.
+     * @return {Object} hashtable (language code -> description)
+     */
+    this.languages = function() {
+      return languages;
+    };
+
+    /**
+     * Get whether the Text-to-Speech audio can be played by the browser.
+     * @return true if so, false if not.
+     */
+    this.canPlay = function(cb) {
+      return canPlayHTML5MP3Audio;
+    };
+
+    /**
+     * Construct the URL to fetch the speech audio for given text and language.
+     * @param txt the text.
+     * @param lang the language of the text. If omitted then default language is used.
+     */
+    this.url = function(txt, lang) {
+      lang = lang || defaultLanguage;
+
+      if (!txt || 0 >= txt.length)
+        throw new Error('Need some text');
+
+      return 'http://translate.google.com/translate_tts?ie=UTF-8&tl=' + lang + '&q=' + txt;
+    };
+
+    /**
+     * Fetch and play the speech audio for given text and language.
+     * @param txt the text.
+     * @param lang the language of the text. If omitted then default language is used.
+     * @param cb Completion callback with signature (err).
+     */
+    this.play = function(txt, lang, cb) {
+      var self = this;
+
+      if (!canPlayHTML5MP3Audio)
+        return cb(new Error('Sorry, your browser cannot play HTML5 MP3 Audio.'));
+
+      // load the MP3
+      try {
+        var audio = new Audio();
+        audio.src = self.url(txt, lang);
+        audio.play();
+
+        cb();
+      } catch (e) {
+        return cb(e);
+      }
+    };
+  }
+});
